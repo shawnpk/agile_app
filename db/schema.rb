1fffc1af947f88cf31c8f8168395ca3a69a476ff
Shawn Kearney <shawnpk@ineptsoftware.com>
2018-03-31 11:43:42 -0700

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330205832) do
+ActiveRecord::Schema.define(version: 20180331183837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180330205832) do
     t.index ["settings"], name: "index_accounts_on_settings", using: :gin
   end
 
-  create_table "users", id: :bigserial, force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
@@ -47,12 +47,9 @@ ActiveRecord::Schema.define(version: 20180330205832) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.bigint   "account_id"
-    t.index ["account_id"], name: "index_users_on_account_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["hash_id"], name: "index_users_on_hash_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "users", "accounts"
 end
